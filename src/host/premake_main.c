b6a8aaffb2349382b0129f2dcc811237b297af7c
sound <sound@sagaforce.com>
2016-02-21 23:47:48 -0800

    Added pledge call.

--- src/host/premake_main.c
+++ src/host/premake_main.c
@@ -3,13 +3,28 @@
  * \brief  Program entry point.
  * \author Copyright (c) 2002-2013 Jason Perkins and the Premake project
  */
-
+#ifdef __OpenBSD__
+#  include <stdlib.h>
+#  include <unistd.h>
+#  include <sys/param.h>
+#endif
 #include "premake.h"
 
 int main(int argc, const char** argv)
 {
 	lua_State* L;
 	int z;
+
+#ifdef OpenBSD5_9
+	if(pledge("stdio rpath wpath cpath exec proc"
+#ifdef PREMAKE_CURL
+		" inet"
+#endif
+		, NULL) < 0) {
+		perror("premake5");
+		exit(-1);
+	}
+#endif
 
 	L = luaL_newstate();
 	luaL_openlibs(L);
